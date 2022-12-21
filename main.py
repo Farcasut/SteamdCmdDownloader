417da73956bf5206ba5e67412db9dacdf5e87e37
Farcasut <92780090+Farcasut@users.noreply.github.com>
2022-12-22 01:45:34 +0200

    Second Version

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 
 import string
 import subprocess
+import re
+import requests
 path:string = "./steamcmd.exe"
-
-
-
 
 def startSubprocess(gameID, modID, gameName):
     steamcmd_params = (
         path,
-        '+force_install_dir {}'.format(gameName),
+        '+force_install_dir {}'.format("./Downloads/"+gameName),
         '+login {} {}'.format("anonymous", None),
         '+workshop_download_item {} {}'.format(gameID, modID),
         '{}'.format(None),
@@ -19,44 +18,29 @@ def startSubprocess(gameID, modID, gameName):
     print("\n\n")
 
 
-def process_link(gameID,gameName):
+def getGameData(link):
+    htmlSteam =  requests.get(link)
+    regexGameID = "(https:\/\/steamcommunity\.com\/app\/[0-9]+) ?"
+    regexGameName="<div class=\"apphub_AppName ellipsis\">.*<.div>"
+
+    linkHomePage = re.search(regexGameID, htmlSteam.text)[0]
+    return [linkHomePage[linkHomePage.find("/app/")+5:], re.search(regexGameName, htmlSteam.text)[0][37:-6]]
+
+def process_link():
     print("quit->quit\nchange game ->change\n")
     while True:
         linkWorkShop = input("Link of the mod:\n")
         if linkWorkShop.upper()=="QUIT":
-           return
-        if linkWorkShop.upper() == "CHANGE":
-            break;
+            return;
+
+        [gameID,gameName]= getGameData(linkWorkShop)
         modID = linkWorkShop[linkWorkShop.find("?id=")+4:]
-        startSubprocess(gameID, modID, gameName)
-    initialize()
+        startSubprocess(gameID, modID,gameName)
     return;
 
-
-
-def initialize():
-    gameList = {}
-    with open("gamesID.txt") as ID:
-        for line in ID:
-            (name, id) = line.split()
-            gameList[name.upper()] = id
-
-    try:
-        gameName = input("GAME NAME:\n").upper();
-        process_link(gameList[gameName], gameName)
-    except KeyError:
-        print("\n\nThe game isn't present in the gamesID file.\n\n");
-        userDecision = input("\nDo you want to add the game? yes/no\n");
-        if userDecision.upper() == "YES":
-            gameName = input("The name of the game or an alias\n").upper();
-            gameID = input(
-                "Game ID. You can find the game on the home workshop of the game. Eg: https://steamcommunity.com/app/GAME_ID/workshop/\n")
-            with open("gamesID.txt", 'a') as ID:
-                ID.write('\n' + gameName + " " + gameID)
-            process_link(gameID, gameName)
-
 def main():
-    initialize()
+    process_link()
+
 
 if __name__ == '__main__':
    main()
